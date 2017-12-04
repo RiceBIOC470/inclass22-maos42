982f5c80012e49a4ea29a95a7fbf2ca08659abd1
gbri140 <glb3@rice.edu>
2017-12-03 22:07:14 -0600

    Update Inclass22.m

--- Inclass22.m
+++ Inclass22.m
@@ -1,5 +1,53 @@
 %Inclass 22
 
+
+%GB comments
+1 50 Final graphs are all the same graph but just different sections. Please see this script to directly address the problem. 
+figure; hold on; 
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, 1-kb, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =1')
+ 
+figure; hold on;  
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, -kb,0,0,1, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =4')
+hold off 
+ 
+figure; hold on;  
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, -kb,0,0,0,0,0,0,1, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =8')
+hold off 
+
+2 80. A bit closer but still incorrect. Please see below
+figure; hold on; 
+kb = 0; 
+for ku = 0:0.05:5
+    polycoeff = [1, -ku, +1, -kb] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(ku*ones(length(rts),1),rts,'r.');
+end 
+title('n =2')
+
+overall 65
+
 %1. Consider the case of the auto-activating gene that we discussed in class
 %today. Make a bifurcation diagram for this system by varying the
 %activated transcription rate for three cases - in which 1, 4, or 8 copies of the
